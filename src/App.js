9871ba0ff66292a27b835b2f59aa288dfe425d51
yt <aiueo>
2022-05-25 08:45:57 +0900

    reset用のcssをnpmで追加、適用。reset.cssの削除

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Reset } from 'styled-reset';
 import { Header } from './components/index.js'
 
 function App() {
   return (
     <div className="App">
+      <Reset />
       <Header />
     </div>
   );
