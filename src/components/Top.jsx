bcd177a56b6b826dc83fd414ae186d6e97648788
yt <aiueo>
2022-05-28 09:27:51 +0900

    Top画像部分を作成

---
+++ src/components/Top.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import bgImageForMobile from '../img/fv-bgi_sp@2x.jpg'
+
+const Top = () => {
+  return (
+    <StyledTopContainer bgImage={bgImageForMobile}>
+        <StyledSquare>
+            <StyledTitle>Create Design.</StyledTitle>
+        </StyledSquare>
+    </StyledTopContainer>
+  )
+}
+
+export default Top
+
+const StyledTopContainer = styled.section`
+    background-image: url(${props => props.bgImage});
+    background-size: cover;
+    height: 590px;
+    padding: 0 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const StyledSquare = styled.section`
+    width: 300px;
+    height: 100px;
+    border: solid white 2px;
+`
+
+const StyledTitle = styled.h2`
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    font-size: 30px;
+    font-style: normal;
+    color: white;
+    height: 40px;
+    width: 219px;
+    text-align: center;
+    line-height: 104px;
+    margin: 0 auto;
+`
