c2d02492fcfdc18f7f78bdc15e6d94bdcccbe6b3
yt <aiueo>
2022-06-04 17:35:35 +0900

    AboutとServiceのコンポーネント生成時に表示されていた警告を解消。それに伴いAboutとServiceの情報用の新規ファイルを作成

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Title, UnitInfoWrap } from './index'
+import aboutList from '../about'
 
 const About = () => {
-    let summary = 'ミニマルで\n洗練されたデザインを';
-    let detail = `
-        近年、ミニマルなデザインが流行しています。 
-        そこで弊社では、
-        クライアント企業様新規サービス等の課題に対してミニマルで洗練されたデザインを実現させることで解決のサポートを致します。 
-        もちろん全てのサービスにおいてミニマルなデザインが課題解決になるわけではないので、 
-        課題や今後のサービスの展開等しっかりとヒアリングを行なった上でご提案させて頂きます。
-    `
-    let infoList = [[summary, detail]]
   return (
     <StyledAbout>
         <Title title='About' />
-        <UnitInfoWrap infoList={infoList} />
+        <UnitInfoWrap infoList={aboutList} />
     </StyledAbout>
   )
 }
---
+++ src/service.js
@@ -0,0 +1,20 @@
+import img1 from './img/service-image@2x.jpg'
+import img2 from './img/service-image02@2x.jpg'
+
+const summary1 = 'リリース時のサポートで\nサービスのブランディングを';
+const detail1 = `
+    弊社では、リリース時もサポートさせて頂きます。 
+    プレスリリース用のサイトや動画制作を通して、サービスのブランディングを行わせて頂きます。
+`
+const summary2 = 'リリース後のサポートで\n効果を最大化させる';
+const detail2 = `
+    弊社では、リリース後もサポートさせて頂きます。 
+    サービスはリリース後に様々な課題にぶつかります。 
+    そこでクライアント様と一緒に改善を行うことで、デザインの効果を最大化させます。
+`
+const serviceList = {
+    service1: {summary: summary1, detail: detail1, img: img1},
+    service2: {summary: summary2, detail: detail2, img: img2},
+}
+
+export default serviceList
